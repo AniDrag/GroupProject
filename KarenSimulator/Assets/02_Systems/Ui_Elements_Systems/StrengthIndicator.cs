b914aa5cb989fd6241c7fc112c7bd4d3ba5d0b15
AmberSparreboom <ambersparreboom@outlook.com>
2025-01-21 12:39:55 +0100

    main_3.2.12
    SliderPrefabsStrengthIndicator

--- KarenSimulator/Assets/02_Systems/Ui_Elements_Systems/StrengthIndicator.cs
+++ KarenSimulator/Assets/02_Systems/Ui_Elements_Systems/StrengthIndicator.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,23 +7,79 @@ public class StrengthIndicator : MonoBehaviour
 {
     [SerializeField] Slider strengthMeter; // Reference to the slider
     public float speedMeter = 1f; // Speed of movement
-    private bool movingRight = true; // Direction flag
+
     public bool isMoving;
     [Range(0.1f, 10)]
     public float speedTransfer;
+    public bool isActive;
+    private bool isMovingRight;
+    public enum MeterType
+    {
+        none,
+        strengthmeter, 
+        dangermeter
+    }
+
+    public MeterType meterType;  
     private void Start()
     {
         speedMeter = 100 / speedTransfer;
+
     }
     void Update()
     {
-
-        strengthMeter.value += speedMeter * Time.deltaTime;
-        if(strengthMeter.value == strengthMeter.maxValue)
+        if (!isActive)
         {
-            //explode hand
-            //Debug.Log("animation ExplodeHand is playing");
+            return;
+        }
+        if (meterType == MeterType.strengthmeter)
+        {
+            StrengthMeter();
         }
 
+        else if (meterType == MeterType.dangermeter)
+        {
+            DangerMeter();
+        }
+
+        else
+        {
+            Debug.LogWarning("No sliderType selected");
+        }
+
+    }
+    void StrengthMeter()
+    {
+        if (strengthMeter == null) return;
+        // Move the slider value
+        if (isMovingRight)
+        {
+            strengthMeter.value += speedMeter * Time.deltaTime;
+            if (strengthMeter.value >= strengthMeter.maxValue)
+            {
+                isMovingRight = false;
+            }
+
+        }
+        else
+        {
+            strengthMeter.value -= speedMeter * Time.deltaTime;
+            if (strengthMeter.value <= strengthMeter.minValue)
+            {
+                isMovingRight = true;
+            }
+
+        }
+
+
+    }
+
+    void DangerMeter()
+    {
+        strengthMeter.value += speedMeter * Time.deltaTime;
+        if (strengthMeter.value == strengthMeter.maxValue)
+        {
+            Debug.Log("Destroy hand");
+        }
     }
 }
